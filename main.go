33bd3e1e8a20e298576130e04bcf0b58dcc32104
Leandro Gaspar Scanavini <leandrogaspar789@gmail.com>
2018-10-28 11:22:13 -0300

    Fixing listening port

--- main.go
+++ main.go
@@ -11,12 +11,14 @@ func main() {
 	http.Handle("/", fs)
 
 	log.Println("Listening...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(GetPort(), nil)
 }
 
 // Get the port environment variable
 func GetPort() string {
+	log.Println("Loading env port...")
 	var port = os.Getenv("PORT")
+	log.Println("Env port: " + port)
 	if port == "" {
 		port = "3000"
 		log.Println("No PORT variable was set. Using default port: " + port)
